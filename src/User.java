9de248bef830041f43c0df864e26668a36ac69e1
KD0AZT <kd0azt@gmail.com>
2021-03-14 14:10:38 -0700

    User.java should be ready to integrate into the main project

--- src/User.java
+++ src/User.java
@@ -1,43 +1,55 @@
-
+package com.sudoku.models;
 import java.util.Scanner;
 
-public class User {
-    
-    String userName;
-    int userScore;
-    
-    public void setName() {
+public class Help {
+
+    //print out the possible values for the selected point.
+    public static void movesRemaining(MainBoard board, Subboard subboard, Point point) {
+    	
+        int i;
+        int j;
+        int k;
+        int x = point.rowLocation;
+        int y = point.columnLocation;
         
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Enter Username: ");
-        userName = keyboard.next();
-        keyboard.close();
+        //represents the numbers 1 through 9, to indicate whether they are valid at the current location.
+        boolean[] exists = {false, false, false, false, false, false, false, false, false};
         
+        //todo: combine the board row, column, and subboard into a single array to analyze for missing values.
+        for (i = 0; i < 9; i++) {
+            temp[i] = board[i][x];
+        }
+        
+        for (i = 0; i < 9; i++) {
+            temp[(i + 9)] = board[y][i];
+        }
+        
+        for (i = 0; i < 3; i++) {
+            temp[(i + 18)] = subboard[0][i];
+        }
+        
+        for (i = 0; i < 3; i++) {
+            temp[(i + 21)] = subboard[1][i];
+        }
+        
+        for (i = 0; i < 3; i++) {
+            temp[(i + 24)] = subboard[2][i];
+        } 
+        
+        //iterate through the temp array to find 1-9.
+        for (i = 0; i < exists.length; i++) {
+            for (j = 0; j < temp.length; j++) {
+                if (temp[j] == i + 1) {
+                    exists[i] = true;
+                }
+            }
+        }
+        
+        //print the missing values.
+        for (i = 0; i < exists.length; i++) {
+            if (exists[i] == false) {
+                System.out.print((i + 1) + " ");
+            }
+        }
     }
-    
-    public String getName() {
-        
-        return userName;
-        
-    }
-    
-    public void setScore() {
-        
-        userScore++;
-        
-    }
-    
-    public int getScore() {
-    
-        return userScore;
-    
-    }
-    
-    public int movesRemaining() {
-    
-    	//the goal of this class is to analyze the current values in the column, row, and 
-    	// subgrid of the selected box and compute the possible values that would satisfy them.
-    	return 0;
-    }
-    
 }
